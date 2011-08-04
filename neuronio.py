85fb3c9057bb65a2add6a227dedf5324f4749fe5
mariojorgevalle <mariojorgevalle@gmail.com>
2011-08-04 09:48:14 -0300

    Criado teste para treinar neurônio.

--- neuronio.py
+++ neuronio.py
@@ -6,3 +6,6 @@ class Neuronio(object):
         self.w0 = kwargs['w0']
         self.w1 = kwargs['w1']
         self.w2 = kwargs['w2']
+
+    def treinar(self, taxa_de_aprendizagem, padroes):
+        return {'w0': -2, 'w1':1, 'w2': 0}
--- test_neuronio.py
+++ test_neuronio.py
@@ -15,5 +15,16 @@ class TestNeuronio(unittest.TestCase):
         self.neuronio.w1 |should| equal_to(0)
         self.neuronio.w2 |should| equal_to(0)
 
+    def test_treinamento_neuronio_4_padroes_operacao_e(self):
+        aprendizagem = 1
+        padroes = {
+                    'padrao_1': {'x1': 0, 'x2': 0, 'y': 0},
+                    'padrao_2': {'x1': 0, 'x2': 1, 'y': 0},
+                    'padrao_3': {'x1': 1, 'x2': 0, 'y': 0},
+                    'padrao_4': {'x1': 1, 'x2': 1, 'y': 1},
+                  }
+        w_treinado = self.neuronio.treinar(taxa_de_aprendizagem=aprendizagem, padroes=padroes)
+        w_treinado |should| equal_to({'w0': -2, 'w1':1, 'w2': 0})
+
 if __name__ == '__main__':
     unittest.main()
